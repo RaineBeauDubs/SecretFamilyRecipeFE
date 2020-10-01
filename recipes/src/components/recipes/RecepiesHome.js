6db9bfcfeb016791ce59b7054e019abef56f46e4
Raine Wallace <rainebeaudubs@gmail.com>
2020-10-01 00:21:08 -0400

    added CDM/get req for all recipes

--- recipes/src/components/recipes/RecepiesHome.js
+++ recipes/src/components/recipes/RecepiesHome.js
@@ -1,7 +1,25 @@
+import axios from 'axios';
 import React from 'react';
 import requiresAuth from '../auth/requiresAuth';
 
 class RecipesHome extends React.Component {
+  state = {
+    recipes = []
+  }
+
+  componentDidMount = () => {
+    axios
+      .get('http://localhost:5000/api/recipes')
+      .then(response => {
+        this.setState({
+          recipes: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   render() {
     return (
       <div>
