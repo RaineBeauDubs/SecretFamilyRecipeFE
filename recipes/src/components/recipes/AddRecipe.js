6e5a10c9b497907b5de5b26345f19f9adb322aa3
Raine Wallace <rainebeaudubs@gmail.com>
2020-10-05 20:53:20 -0400

    added form to add recipe

--- recipes/src/components/recipes/AddRecipe.js
+++ recipes/src/components/recipes/AddRecipe.js
@@ -33,7 +33,45 @@ class AddStory extends React.Component {
   render() {
     return (
       <div>
-        
+        <h2>Add Recipe:</h2>
+        <form onSubmit={this.addNewStory}>
+          <input 
+            type='text'
+            name='title'
+            placeholder='Title'
+            value={this.state.title}
+            onChange={this.handleChange}
+          />
+          <input 
+            type='text'
+            name='source'
+            placeholder='Source'
+            value={this.state.source}
+            onChange={this.handleChange}
+          />
+          <input 
+            type='text'
+            name='ingredients'
+            placeholder='Ingredients'
+            value={this.state.ingredients}
+            onChange={this.handleChange}
+          />
+          <input 
+            type='text'
+            name='instructions'
+            placeholder='Instructions'
+            value={this.state.instructions}
+            onChange={this.handleChange}
+          />
+          <input 
+            type='text'
+            name='category'
+            placeholder='Category'
+            value={this.state.category}
+            onChange={this.handleChange}
+          />
+          <button type='submit'>Add recipe!</button>
+        </form>
       </div>
     )
   }
