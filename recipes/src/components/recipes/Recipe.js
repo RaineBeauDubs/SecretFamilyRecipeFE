81eb909295c48122773b37e7dc9a0b3937200ea2
Raine Wallace <rainebeaudubs@gmail.com>
2020-11-21 16:22:56 -0500

    added and tested update toggle

--- recipes/src/components/recipes/Recipe.js
+++ recipes/src/components/recipes/Recipe.js
@@ -13,6 +13,9 @@ const reqOps = {
 class Recipe extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isActive: false
+    }
   }
 
   deleteRecepe() {
@@ -22,7 +25,14 @@ class Recipe extends React.Component {
       .catch(error => console.log(error))
   }
 
+  toggleClassName = () => {
+    this.setState({
+      isActive: !this.state.isActive
+    })
+  }
+
   render() {
+    const isActive = this.state.isActive;
     return (
       <div className='recipeCont'>
         <div>
@@ -34,17 +44,20 @@ class Recipe extends React.Component {
         <p>{this.props.instructions}</p>
         <p>{this.props.category}</p>
         <p>{this.props.userId}</p>
+        <div className={isActive ? "updateClose" : "updateOpen"}>
+          <button className='fadeBttn' onClick={() => this.toggleClassName()}>Update Recipe</button>
+          <UpdateRecipe
+            recipe={this.props.recipe}
+            id={this.props.id}
+            title={this.props.title}
+            source={this.props.source}
+            ingredients={this.props.ingredients}
+            instructions={this.props.instructions}
+            category={this.props.category}
+            userId={this.props.user_id}
+          />
+        </div>
 
-        <UpdateRecipe
-          recipe={this.props.recipe}
-          id={this.props.id}
-          title={this.props.title}
-          source={this.props.source}
-          ingredients={this.props.ingredients}
-          instructions={this.props.instructions}
-          category={this.props.category}
-          userId={this.props.user_id}
-        />
       </div>
     )
   }
